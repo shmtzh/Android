aa61be6c8ce5445f068a12731d6f2dd9568a6d49
shmtzh <AmericanDon1994>
2016-02-24 19:01:48 +0200

    first prototype of login

--- app/src/main/java/com/example/shmtzh/myapplication/activity/LoginActivity.java
+++ app/src/main/java/com/example/shmtzh/myapplication/activity/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.shmtzh.myapplication;
+package com.example.shmtzh.myapplication.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.shmtzh.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
 
